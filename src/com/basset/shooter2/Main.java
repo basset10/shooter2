b2f267369c43ba899c82724704da1e5140b33a2b
Zack <Zack@192.168.180.39>
2018-04-21 21:49:34 -0500

    added things

--- src/com/basset/shooter2/Main.java
+++ src/com/basset/shooter2/Main.java
@@ -1,5 +1,9 @@
 package com.basset.shooter2;
 
+import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.hvlDrawQuadc;
+
+import org.newdawn.slick.Color;
+
 import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
 import com.osreboot.ridhvl.template.HvlTemplateInteg2D;
 
@@ -8,6 +12,8 @@ public class Main extends HvlTemplateInteg2D {
 	public static void main (String args[]){
 		new Main();
 	}
+	
+	Player player;
 
 	public Main(){
 		super(144, 1280, 720, "SHHHHHHHOOOOOOOOOOOOOOOOOOOOTER2", new HvlDisplayModeDefault());
@@ -15,11 +21,15 @@ public class Main extends HvlTemplateInteg2D {
 
 	@Override
 	public void initialize(){
+		
+		player = new Player(1280/2, 720/2);
 
 	}
 
 	@Override
 	public void update(float delta){
+		
+		hvlDrawQuadc(player.getxPos(), player.getyPos(), Player.PLAYER_SIZE, Player.PLAYER_SIZE, Color.blue);
 
 	}
 
