fba74dbe764f230a9be4f8b067877a346d1dfeb0
osreboot <calvin.16.weaver@gmail.com>
2018-04-21 21:54:15 -0400

    Fixed

--- src/com/basset/shooter2/Main.java
+++ src/com/basset/shooter2/Main.java
@@ -1,35 +1,27 @@
 package com.basset.shooter2;
 
-import com.osreboot.ridhvl.display.HvlDisplayMode;
 import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
 import com.osreboot.ridhvl.template.HvlTemplateInteg2D;
 
 public class Main extends HvlTemplateInteg2D {
 
-	public Main() {
-		super(144, 1280, 720, "SHHHHHHHOOOOOOOOOOOOOOOOOOOOTER2", new HvlDisplayModeDefault()  );
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main (String args[]) {
-		
-		
+	public static void main (String args[]){
 		new Main();
-		
-		
+	}
+
+	public Main(){
+		super(144, 1280, 720, "SHHHHHHHOOOOOOOOOOOOOOOOOOOOTER2", new HvlDisplayModeDefault());
 	}
 
 	@Override
-	public void initialize() {
-		// TODO Auto-generated method stub
-		
+	public void initialize(){
+
 	}
 
 	@Override
-	public void update(float delta) {
-		// TODO Auto-generated method stub
-		
+	public void update(float delta){
+
 	}
-	
-	
+
+
 }
