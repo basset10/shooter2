0a9010af1ee7577781e8e3d2be7e9b7ff9799f8f
Zack <Zack@192.168.180.39>
2018-04-21 20:58:53 -0500

    anything

--- src/com/basset/shooter2/Main.java
+++ src/com/basset/shooter2/Main.java
@@ -1,5 +1,35 @@
 package com.basset.shooter2;
 
-public class Main {
+import com.osreboot.ridhvl.display.HvlDisplayMode;
+import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
+import com.osreboot.ridhvl.template.HvlTemplateInteg2D;
 
+public class Main extends HvlTemplateInteg2D {
+
+	public Main() {
+		super(144, 1280, 720, "SHHHHHHHOOOOOOOOOOOOOOOOOOOOTER2", new HvlDisplayModeDefault()  );
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main (String args[]) {
+		
+		
+		new Main();
+		
+		
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
