9963cb9f302699bd6356bd0d68d09a8ff90b4e97
Zack <Zack@192.168.180.39>
2018-04-24 21:29:26 -0500

    t5eyr6ut7i8o9poi

--- src/com/basset/shooter2/Player.java
+++ src/com/basset/shooter2/Player.java
@@ -2,6 +2,8 @@ package com.basset.shooter2;
 
 import org.lwjgl.input.Keyboard;
 
+import com.osreboot.ridhvl.HvlMath;
+
 public class Player {
 
 	public static final float PLAYER_SIZE = 20f;
@@ -23,30 +25,42 @@ public class Player {
 	}
 
 	public void update(float delta) {
+		
+		if(Keyboard.isKeyDown(Keyboard.KEY_W) || Keyboard.isKeyDown(Keyboard.KEY_S) || Keyboard.isKeyDown(Keyboard.KEY_D) || Keyboard.isKeyDown(Keyboard.KEY_A)) {
+			
+			ySpeed = 0;
+			xSpeed = 0;
+			
+		}else {
+			
+			ySpeed = HvlMath.stepTowards(ySpeed, delta * 1500, 0);
+			xSpeed = HvlMath.stepTowards(xSpeed, delta * 1500, 0);
+			
+		}
 
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
-			ySpeed = ySpeed - PLAYER_SPEED;
+			ySpeed = HvlMath.stepTowards(xSpeed, delta * 10000, -PLAYER_SPEED);
 		}
 
 
-		if(Keyboard.isKeyDown(Keyboard.KEY_S)) {
-			ySpeed = ySpeed + PLAYER_SPEED;
+		 if(Keyboard.isKeyDown(Keyboard.KEY_S)) {
+			ySpeed = HvlMath.stepTowards(ySpeed, delta * 10000, PLAYER_SPEED);
 		}
 
 
 		if(Keyboard.isKeyDown(Keyboard.KEY_D)) {
-			xSpeed = xSpeed + PLAYER_SPEED;
+			xSpeed = HvlMath.stepTowards(xSpeed, delta * 10000, PLAYER_SPEED);
 		}
 
 		if(Keyboard.isKeyDown(Keyboard.KEY_A)) {
-			xSpeed = xSpeed - PLAYER_SPEED;
+			xSpeed = HvlMath.stepTowards(xSpeed, delta * 10000, -PLAYER_SPEED);
 		}
 
 		yPos = yPos+(delta * ySpeed);
 		xPos = xPos+(delta * xSpeed);
 
-		ySpeed = 0;
-		xSpeed = 0;
+		
+	
 	}
 
 	public float getxPos(){
