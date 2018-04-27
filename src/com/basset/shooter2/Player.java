03e93b12af898b473d1c38677e27d617ecb57f72
Zack <Zack@192.168.180.39>
2018-04-27 00:55:14 -0500

    MOVEMENT MORE GOODER

--- src/com/basset/shooter2/Player.java
+++ src/com/basset/shooter2/Player.java
@@ -1,15 +1,16 @@
 package com.basset.shooter2;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
 
 import com.osreboot.ridhvl.HvlMath;
 
 public class Player {
 
 	public static final float PLAYER_SIZE = 20f;
-	public static final int PLAYER_SPEED = 250;
-	public static final int PLAYER_ACCELERATION = 100;
-	public static final int PLAYER_DECELERATION = 1000;	
+	public static int maxSpeed = 260;
+	public static final int PLAYER_ACCELERATION = 120;
+	public static final int PLAYER_DECELERATION = 1100;	
 
 	private float xPos;
 	private float yPos;
@@ -29,45 +30,43 @@ public class Player {
 
 	public void update(float delta) {
 		
-		acceleration = HvlMath.stepTowards(acceleration, delta*90000, ((HvlMath.distance(0, 0, xSpeed, ySpeed)) * PLAYER_ACCELERATION) + 100);
-
+		acceleration = HvlMath.stepTowards(acceleration, delta*97500, ((HvlMath.distance(0, 0, xSpeed, ySpeed)) * PLAYER_ACCELERATION) + 100);
+		
+		if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)){
+			maxSpeed = 400;
+		}else {
+			maxSpeed = 260;
+		}
 
 		if(!Keyboard.isKeyDown(Keyboard.KEY_W) && !Keyboard.isKeyDown(Keyboard.KEY_S)) {
-						
 			ySpeed = HvlMath.stepTowards(ySpeed, delta * PLAYER_DECELERATION, 0);
-			
 		}
 
 		if(!Keyboard.isKeyDown(Keyboard.KEY_D) && !Keyboard.isKeyDown(Keyboard.KEY_A)) {
-
 			xSpeed = HvlMath.stepTowards(xSpeed, delta * PLAYER_DECELERATION, 0);
-			
 		}
 
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
-			
-			ySpeed = HvlMath.stepTowards(ySpeed, acceleration * delta, -PLAYER_SPEED);
+			ySpeed = HvlMath.stepTowards(ySpeed, acceleration * delta, -maxSpeed);
 		}
 
 
 		if(Keyboard.isKeyDown(Keyboard.KEY_S)) {
-			
-			ySpeed = HvlMath.stepTowards(ySpeed, acceleration * delta, PLAYER_SPEED);
+			ySpeed = HvlMath.stepTowards(ySpeed, acceleration * delta, maxSpeed);
 		}
 
 
 		if(Keyboard.isKeyDown(Keyboard.KEY_D)) {
-			
-			xSpeed = HvlMath.stepTowards(xSpeed, acceleration * delta, PLAYER_SPEED);
+			xSpeed = HvlMath.stepTowards(xSpeed, acceleration * delta, maxSpeed);
 		}
 
 		if(Keyboard.isKeyDown(Keyboard.KEY_A)) {
-			
-			xSpeed = HvlMath.stepTowards(xSpeed, acceleration * delta, -PLAYER_SPEED);
+			xSpeed = HvlMath.stepTowards(xSpeed, acceleration * delta, -maxSpeed);
 		}
 
-		yPos = yPos+(delta * ySpeed);
-		xPos = xPos+(delta * xSpeed);
+		
+		yPos = HvlMath.limit(yPos+(delta * ySpeed), 0, Display.getHeight());
+		xPos = HvlMath.limit(xPos+(delta * xSpeed), 0, Display.getWidth());
 
 System.out.println(acceleration);
 
